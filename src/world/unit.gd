ebe8f3b386b4bf3f66509547591e0ef739e5b734
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-03-22 03:35:12 +0100

    units

---
+++ src/world/unit.gd
@@ -0,0 +1,7 @@
+class_name Unit
+
+var unit_data: UnitData
+var moves_left: int
+var move_fractions_left: int	# for road movement
+
+var faction: Faction
---
+++ src/world/unit_stack.gd
@@ -0,0 +1,9 @@
+class_name UnitStack
+
+
+var coords: Vector2i
+var units: Array[Unit]
+var surface: WorldSurface
+
+func can_move_dir(dir: Vector2i)-> bool:
+	
