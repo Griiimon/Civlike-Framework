dd14ce81a7d0ebbe1a4184a13b9d7415c35b7ca8
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-03-25 15:15:15 +0100

    spawn unit

--- src/world/world_surface.gd
+++ src/world/world_surface.gd
@@ -7,7 +7,7 @@ var height: int
 
 var tiles: Array2D
 
-var units: Array[Unit]
+var unit_stacks: Array[UnitStack]
 
 var world: World
 
@@ -25,6 +25,25 @@ func generate_default_tiles():
 	for x in width:
 		for y in height:
 			tiles.put(x, y, GameData.terrains[0])
+
+func spawn_unit(_unit_data: UnitData, _pos: Vector2i, faction: Faction)-> UnitStack:
+	var unit_stack:= UnitStack.new(self, _pos, faction)
+	unit_stack.add_unit(Unit.new(_unit_data))
+	assert(unit_stack.can_stand_on(_pos))
+	
+	return unit_stack
+
+func get_unit_stacks_at(_target_pos)-> Array[UnitStack]:
+	var result: Array[UnitStack]
+	
+	for stack in unit_stacks:
+		if stack.pos == _target_pos:
+			result.append(stack)
+	
+	return result
+
+func get_terrain_at(_pos: Vector2i)-> TerrainData:
+	return tiles.readv(_pos)
 
 func is_empty()-> bool:
 	return not tiles
