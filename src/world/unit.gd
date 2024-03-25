71d41f4cdac2af5699f1e5960c37a9d5d54f2826
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-03-25 15:15:42 +0100

    init

--- src/world/unit.gd
+++ src/world/unit.gd
@@ -5,5 +5,10 @@ var moves_left: int
 var move_fractions_left: int	# for road movement
 
 
+func _init(_data: UnitData):
+	unit_data= _data
+	moves_left= unit_data.movement
+
+
 func can_enter_terrain(terrain: TerrainData)-> bool:
 	return unit_data.can_enter_terrain(terrain)
