e04292cb7565c602905b28807d927b832a49ced2
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-03-25 15:15:21 +0100

    Update unit_terrain_test.gd

--- tests/unit_terrain_test.gd
+++ tests/unit_terrain_test.gd
@@ -1,5 +1,5 @@
 
-static func run(_world: World):
+static func run(_world: World, _game: Game):
 	for unit_data in GameData.units:
 		for terrain in GameData.terrains:
 			prints(unit_data.get_display_name(), "can enter terrain", terrain.get_display_name(), "?", unit_data.can_enter_terrain(terrain))
