ded316a64de7208b545dc2a81f465ed813df9a8f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-03-25 15:16:15 +0100

    can disable

--- src/test_runner.gd
+++ src/test_runner.gd
@@ -1,11 +1,20 @@
 extends Node
 class_name TestRunner
 
+@export var enabled:= true
 @export var tests: Array[GDScript]
 
 func _ready():
+	if not enabled:
+		return
+		
 	late_ready.call_deferred()
 
 func late_ready():
+	await get_tree().process_frame
+	
+	assert(get_parent() is Game)
+	var game: Game= get_parent()
+	
 	for test in tests:
-		test.new().run(get_parent().world)
+		test.new().run(game.world, game)
