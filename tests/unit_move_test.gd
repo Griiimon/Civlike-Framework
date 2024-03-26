f2e3568950a5fb205cb0b801f3573840c2ed71ad
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-03-26 23:14:10 +0100

    Update unit_move_test.gd

--- tests/unit_move_test.gd
+++ tests/unit_move_test.gd
@@ -7,7 +7,6 @@ static func run(_world: World, _game: Game):
 	#_world.add_stack_to_map(stack)
 
 	var stack: UnitStack= _world.spawn_unit(GameData.units_dict["settler"], Vector2i(1, 1), faction)
-	_world.add_stack_to_map(stack)
 	
 	_world.render()
 	var dir:= Vector2i(1, 0)
