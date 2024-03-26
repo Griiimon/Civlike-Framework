449a69534679a870a31394437b6ed4c78f71b101
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-03-26 01:38:07 +0100

    add detection area

--- src/map/map_unit_stack_2d.gd
+++ src/map/map_unit_stack_2d.gd
@@ -2,6 +2,12 @@ extends MapUnitStack
 class_name MapUnitStack2D
 
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var area: Area2D = %Area2D
+@onready var collision_shape: CollisionShape2D = %CollisionShape2D
+
+
+func _ready():
+	collision_shape.shape.size= Vector2.ONE * Consts.TILE_SIZE / 2
 
 func init(_unit_stack: UnitStack):
 	super(_unit_stack)
@@ -10,3 +16,7 @@ func init(_unit_stack: UnitStack):
 func set_position():
 	var world: MyWorld2D= units.get_world() as MyWorld2D
 	sprite.global_position= world.tilemap.to_local(units.pos * Consts.TILE_SIZE)
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	pass
