a2069f53b22c9b0c617dff3d44f2f85dc77be27b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-03-28 00:20:05 +0100

    world tile -> surface tile

---
+++ src/world/surface_tile.gd
@@ -0,0 +1,9 @@
+extends ExtendableGameObject
+class_name SurfaceTile
+
+
+var terrain: TerrainData
+var feature: TerrainFeatureData
+var improvement: TerrainImprovementData
+var resource: TerrainResourceData
+var road_level: int= 0
--- src/world/world_tile.gd
+++
@@ -1,8 +0,0 @@
-class_name WorldTile
-
-
-var terrain: int
-var feature: int
-var improvement: int
-var resource: int
-var road_level: int= 0
