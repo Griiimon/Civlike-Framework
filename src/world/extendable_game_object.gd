a1c91511b16c0d9ebabe3071b673ba3b1e384c51
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-03-28 03:02:14 +0100

    Update extendable_game_object.gd

--- src/world/extendable_game_object.gd
+++ src/world/extendable_game_object.gd
@@ -3,3 +3,10 @@ class_name ExtendableGameObject
 var vars:= {}
 
 
+func set_var(_name: String, value: Variant):
+	vars[_name]= value
+
+func get_var(_name: String):
+	assert(_name in vars)
+	return vars[_name]
+
