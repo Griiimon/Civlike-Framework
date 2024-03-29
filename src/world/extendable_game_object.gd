ca00e1069f70e77dd4273a78366051674b39a65d
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-03-29 02:49:10 +0100

    Update extendable_game_object.gd

--- src/world/extendable_game_object.gd
+++ src/world/extendable_game_object.gd
@@ -10,8 +10,8 @@ func get_var(_name: String):
 	assert(_name in vars)
 	return vars[_name]
 
-func __index(ref: LuaAPI, index):
-	return get_var(index)
+func __index(ref: LuaAPI, _index: String):
+	return get_var(_index)
 
-func __newindex(ref: LuaAPI, index, value):
-	set_var(index, value)
+func __newindex(_ref: LuaAPI, _index: String, _value):
+	set_var(_index, _value)
